c53234f2ab3d6228f9da8054353cac9dd912829a
Tibold Kandrai <tibold@reactor.localdomain>
2026-03-21 00:20:25 +0100

    Added more language to ts

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- Customize Treesitter
 
 ---@type LazySpec
@@ -10,6 +8,18 @@ return {
       "lua",
       "vim",
       -- add more arguments for adding more treesitter parsers
+      "bash",
+      "json",
+      "yaml",
+      "toml",
+      "markdown",
+      "python",
+      "dockerfile",
+      "ini",
+      "regex",
+      "ssh_config",
+      "git_config",
+      "gitignore",
     },
   },
 }
